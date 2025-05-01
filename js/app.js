4b48ee9cfd02c3376416c660da51d974a8ae8479
muhibilloh-git <muhibillohjamoldinov.01@gmail.com>
2025-05-01 13:48:37 +0500

    lesson7

---
+++ js/app.js
@@ -0,0 +1,93 @@
+{
+  //   // 1
+  //   //   function declaration
+  //   //   function cube(n) {
+  //   //     return n * n * n;
+  //   //   }
+  //   //   console.log(cube(2)); // 8
+  //   //   console.log(cube(5)); // 125
+  //   // function expression
+  //   //   const cube = function (n) {
+  //   //     return n * n * n;
+  //   //   };
+  //   //   console.log(cube(2)); // 8
+  //   //   console.log(cube(5)); // 125
+  //   // arrow function
+  // //   const cube = (n) => n * n * n;
+  // //   console.log(cube(2)); // 8
+  // //   console.log(cube(5)); // 125
+}
+
+{
+  // 2
+  // function Declaration
+  // function checkAge(age) {
+  //     return age >= 18 ? "Voyaga yetgan" : "Voyaga yetmagan";
+  // }
+  // console.log(checkAge(20)); // Voyaga yetgan
+  // console.log(checkAge(15)); // Voyaga yetmagan
+  // function Expression
+  // const checkAge = function(age) {
+  //     return age >= 18 ? "Voyaga yetgan" : "Voyaga yetmagan";
+  // }
+  // console.log(checkAge(20)); // Voyaga yetgan
+  // console.log(checkAge(15)); // Voyaga yetmagan
+  // Arrow Function
+  //   const checkAge = (age) => (age >= 18 ? "Voyaga yetgan" : "Voyaga yetmagan");
+  //   console.log(checkAge(20)); // Voyaga yetgan
+  //   console.log(checkAge(15)); // Voyaga yetmagan
+}
+
+{
+  // 3
+  // function Declaration
+  // function printOdd(n) {
+  //     for (let i = 1; i < n; i += 2) {
+  //         console.log(i);
+  //     }
+  // }
+  // printOdd(10);
+  // function Expression
+  // const printOdd = function(n) {
+  //     for (let i = 1; i < n; i += 2) {
+  //         console.log(i);
+  //     }
+  // }
+  // printOdd(10);
+  // Arrow Function
+  // const printOdd = (n) => {
+  //     for (let i = 1; i < n; i += 2) {
+  //         console.log(i);
+  //     }
+  // }
+  // printOdd(10);
+}
+
+{
+  // 4
+  // function Declaration
+  //   function findMin(a, b, c) {
+  //     if (a <= b && a <= c) return a;
+  //     else if (b <= a && b <= c) return b;
+  //     else return c;
+  //   }
+  //   console.log(findMin(5, 3, 9)); // 3
+  //   console.log(findMin(7, 2, 4)); // 2
+  //   console.log(findMin(1, 1, 1)); // 1
+
+  // function Expression
+  //   const findMin = function (a, b, c) {
+  //     if (a <= b && a <= c) return a;
+  //     else if (b <= a && b <= c) return b;
+  //     else return c;
+  //   };
+
+  //   console.log(findMin(5, 3, 9)); // 3
+
+  // function Arrow
+//   const findMin = (a, b, c) =>
+//     a <= b && a <= c ? a : b <= a && b <= c ? b : c;
+
+//   console.log(findMin(5, 3, 9)); // 3
+}
+
